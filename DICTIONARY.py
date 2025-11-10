5d290ac7a43fd5f6214c2b99e7e946fe97e776ee
ibrahimsisfo <ibrahimsisfo@gmail.com>
2025-11-10 14:25:44 +0800

    Add files via upload

---
+++ DICTIONARY.py
@@ -0,0 +1,16 @@
+# Dictionary
+mahasiswa = ["kurni", "mamud", "nopal", "ibo", "jona"]
+
+# Menampilkan semua nama
+print("Daftar Mahasiswa:", mahasiswa)
+
+# Mengakses 
+print("Mahasiswa pertama:", mahasiswa[0])
+
+# Menambah data 
+mahasiswa.append("alim")
+print("Setelah ditambah:", mahasiswa)
+
+# Menghapus 
+mahasiswa.remove("mamud")
+print("Setelah mamud dihapus:", mahasiswa)
