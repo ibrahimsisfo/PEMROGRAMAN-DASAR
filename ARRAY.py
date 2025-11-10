945f79bb5e08f58c7dd87ca5f7b3d1c5a111e075
ibrahimsisfo <ibrahimsisfo@gmail.com>
2025-11-10 14:58:47 +0800

    Add files via upload

---
+++ ARRAY.py
@@ -0,0 +1,20 @@
+# NAMA : IBRAHIM
+# NIM : D0425013
+# SISTEM INFORMASI B
+# ARRAY
+
+Makanan = ["bakso", "binte", "bakwan", "nasi goreng", "ayam bengawan"]
+
+# Menampilkan semua nama
+print("Daftar Makanan:", Makanan)
+
+# Mengakses 
+print("Makanan pertama:", Makanan[0])
+
+# Menambah data 
+Makanan.append("mie coto makassar")
+print("Setelah ditambah:", Makanan)
+
+# Menghapus 
+Makanan.remove("bakso")
+print("Setelah bakso dihapus:", Makanan)
